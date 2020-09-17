793df8d0e144b9e5ed427bcbd057f6fad3ae0dec
youngho-C <cyh6099@gmail.com>
2020-09-18 01:50:06 +0900

    finish main layout

--- introduce/intro.js
+++ introduce/intro.js
@@ -5,4 +5,18 @@ const icon_bar = document.querySelector('.icon');
 toggleBtn.addEventListener('click', () => {
     menu_bar.classList.toggle('active');
     icon_bar.classList.toggle('active');
-})
+});
+
+const showBtn = document.querySelector('.click_btn');
+const show_base = document.querySelector('.base');
+const show_belong = document.querySelector('.belong');
+const show_activity = document.querySelector('.activity');
+const show_sns = document.querySelector('.sns');
+
+showBtn.addEventListener('click',() =>{
+    show_base.classList.toggle('active');
+    show_belong.classList.toggle('active');
+    show_activity.classList.toggle('active');
+    show_sns.classList.toggle('active');
+
+});
