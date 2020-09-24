d883fc9bdb3b52304707ef42dbf77de33fdf7330
youngho-C <cyh6099@gmail.com>
2020-09-24 21:12:06 +0900

    add weather API

---
+++ weather.js
@@ -0,0 +1,58 @@
+const weather = document.querySelector(".js-weather")
+
+const COORDS = 'coords';
+const API_KEY = "8a9ec30a2155643a638723b6f7dc7abf"
+
+function getWeather(lat,lng){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    )
+        .then(function (response){
+            return response.json();
+        })
+        .then(function (json){
+            const temperature = parseInt(json.main.temp/1);
+            const place = json.name;
+            weather.innerText = `현재 위치의 온도 
+            ${temperature}℃`;
+        });
+
+
+}
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj))
+}
+
+function  handleGeoSuccess(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude,longitude)
+}
+function  handleGeoError(){
+    console.log("cant access")
+}
+
+function  askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess,handleGeoError)
+}
+
+function  loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
+        askForCoords();
+    }else{
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude,parseCoords.longitude)
+    }
+}
+
+function init(){
+    loadCoords();
+}
+
+init();
