f8ea3539c78074c1ec9e40017e780ee2b7403dda
youngho-C <cyh6099@gmail.com>
2020-10-02 16:50:44 +0900

    modify date of trouble shooting board

--- sub-trouble-shooting/write.js
+++ sub-trouble-shooting/write.js
@@ -13,35 +13,56 @@ firebase.initializeApp(firebaseConfig);
 // Get a reference to the database service
 var database = firebase.database();
 
-function writePost(username, title, body) {
+function writePost(username, title, main) {
+    let day = new Date();
+    let today = day.getFullYear() + "/" + (day.getMonth()+1) + "/" + day.getDate();
     // A post entry.
     var postData = {
         username,
         title,
-        body,
+        main,
+        today,
     };
+    console.log(today);
     // Get a key for a new Post.
     const newPostKey = firebase.database().ref().child('posts').push(postData);
-    console.log(newPostKey);
     // return firebase.database().ref().update(updates);
 }
 
 
-var regist_btn = document.querySelector('.r_btn');
+var register_btn = document.querySelector('.r_btn');
 
 var subject = document.querySelector('.subject');
 var author = document.querySelector('.author');
 var main_text = document.querySelector('.main_txt');
 var delete_btn = document.querySelector('.del');
 
-regist_btn.addEventListener('click',function (){
-    writePost(author.value,subject.value,main_text.value);
-    author.value = ""
-    subject.value = ""
-    main_text.value = ""
-    console.log(subject.value);
-});
 
-delete_btn.addEventListener('click',function (){
 
+window.onload = function (){
+    register_btn.addEventListener('click',function (){
+        writePost(author.value,subject.value,main_text.value);
+        author.value = ""
+        subject.value = ""
+        main_text.value = ""
+    });
+}
+
+// delete_btn.addEventListener('click',function (){
+//
+// })
+
+function getPosts(){
+    return; //firebase.database().ref('/users/' + userId);
+}
+const postRef = firebase.database().ref('/posts/').on('value', function (snapshot) {
+    const postData = Object.entries(snapshot.val());
+    let tableContent = document.querySelector(".content")
+    tableContent.innerHTML = "";
+    for(let i = 0; i < postData.length;i++)
+    {
+        const[key,body] = postData[i];
+        tableContent.innerHTML += "<tr><td>" + body.today + "</td> <td>" +
+            "<a href='#'>" + body.title+"</a></td> <td>" + body.username + "</td><td>"+ "<a id='del'>x</a>" + "</td></tr>"
+    }
 })
