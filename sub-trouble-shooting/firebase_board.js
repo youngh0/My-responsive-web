2524d5de7ad4513b3c0711f5216635ccfaffefcd
youngho-C <cyh6099@gmail.com>
2020-10-04 23:42:20 +0900

    게시판 수정,삭제 구현, 디테일 다져야됨.

--- sub-trouble-shooting/firebase_board.js
+++ sub-trouble-shooting/firebase_board.js
@@ -1,26 +1,49 @@
-// // Your web app's Firebase configuration
-// var firebaseConfig = {
-//     apiKey: "AIzaSyCdm4yhjEv2bGVp4d8d8SqtyWTpf7gwds0",
-//     authDomain: "my-info-web.firebaseapp.com",
-//     databaseURL: "https://my-info-web.firebaseio.com",
-//     projectId: "my-info-web",
-//     storageBucket: "my-info-web.appspot.com",
-//     messagingSenderId: "451708761343",
-//     appId: "1:451708761343:web:91d8d889069d3fee3cc8ca"
-// };
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-//
-// // Get a reference to the database service
-// var database = firebase.database();
-// function writePost(username, title, body) {
-//     // A post entry.
-//     var postData = {
-//         username,
-//         title,
-//         body,
-//     };
-//     // Get a key for a new Post.
-//     const newPostKey = firebase.database().ref().child('posts').push(postData);
-//     // return firebase.database().ref().update(updates);
-// }
+var firebaseConfig = {
+    apiKey: "AIzaSyCdm4yhjEv2bGVp4d8d8SqtyWTpf7gwds0",
+    authDomain: "my-info-web.firebaseapp.com",
+    databaseURL: "https://my-info-web.firebaseio.com",
+    projectId: "my-info-web",
+    storageBucket: "my-info-web.appspot.com",
+    messagingSenderId: "451708761343",
+    appId: "1:451708761343:web:91d8d889069d3fee3cc8ca"
+};
+firebase.initializeApp(firebaseConfig);
+
+var nameV, titleV, main_txtV;
+
+function ready(){
+    nameV = document.getElementById('name').value;
+    titleV = document.getElementById('title').value;
+    main_txtV = document.getElementById('main_txt').value;
+}
+
+
+function regist(){
+    let day = new Date();
+    let today = day.getFullYear() + "/" + (day.getMonth()+1) + "/" + day.getDate();
+    ready();
+
+    firebase.database().ref('board/' + titleV).set({
+        username:nameV,
+        title:titleV,
+        main_txt:main_txtV,
+        today,
+    })
+    alert("ok");
+}
+
+function calling(){
+    console.log(firebase.database().ref('board/' + titleV))
+    firebase.database().ref('board/' + document.getElementById('title').value).on('value',function (snapshot){
+        document.getElementById('name').value = snapshot.val().username;
+        document.getElementById('main_txt').value = snapshot.val().main_txt;
+    });
+}
+
+function update(){
+    firebase.database().ref('board/' + document.getElementById('title').value).update({
+        title:document.getElementById('title').value,
+        username:document.getElementById('name').value,
+        main_txt:document.getElementById('main_txt').value
+    })
+}
--- sub-trouble-shooting/write.js
+++ sub-trouble-shooting/write.js
@@ -1,3 +1,4 @@
+// Your web app's Firebase configuration
 var firebaseConfig = {
     apiKey: "AIzaSyCdm4yhjEv2bGVp4d8d8SqtyWTpf7gwds0",
     authDomain: "my-info-web.firebaseapp.com",
@@ -10,59 +11,34 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Get a reference to the database service
-var database = firebase.database();
-
-function writePost(username, title, main) {
-    let day = new Date();
-    let today = day.getFullYear() + "/" + (day.getMonth()+1) + "/" + day.getDate();
-    // A post entry.
-    var postData = {
-        username,
-        title,
-        main,
-        today,
-    };
-    console.log(today);
-    // Get a key for a new Post.
-    const newPostKey = firebase.database().ref().child('posts').push(postData);
-    // return firebase.database().ref().update(updates);
-}
-
-
-var register_btn = document.querySelector('.r_btn');
-
-var subject = document.querySelector('.subject');
-var author = document.querySelector('.author');
-var main_text = document.querySelector('.main_txt');
-var delete_btn = document.querySelector('.del');
-
-
-
-window.onload = function (){
-    register_btn.addEventListener('click',function (){
-        writePost(author.value,subject.value,main_text.value);
-        author.value = ""
-        subject.value = ""
-        main_text.value = ""
+// 데티어 불러와서 보여주기
+function show(){
+    firebase.database().ref('board').once('value', function (snapshot) {
+        let test_box = document.querySelector("#test");
+        const postData = Object.entries(snapshot.val());
+        for(let i = 0; i < postData.length;i++)
+        {
+            const[key,body] = postData[i];
+            if(document.getElementById('num').value === body.title){
+                test_box.innerHTML = "<h1>" + body.title + "</h1><div id='bo'>" + body.main_txt +"</div>";
+            }
+        }
     });
 }
 
-// delete_btn.addEventListener('click',function (){
-//
-// })
-
-function getPosts(){
-    return; //firebase.database().ref('/users/' + userId);
+function remove(){
+    console.log(firebase.database().ref('board/'+document.getElementById('num').value));
+    firebase.database().ref('board/'+document.getElementById('num').value).remove();
 }
-const postRef = firebase.database().ref('/posts/').on('value', function (snapshot) {
+
+firebase.database().ref('board').on('value', function (snapshot) {
     const postData = Object.entries(snapshot.val());
     let tableContent = document.querySelector(".content")
     tableContent.innerHTML = "";
     for(let i = 0; i < postData.length;i++)
     {
         const[key,body] = postData[i];
-        tableContent.innerHTML += "<tr><td>" + body.today + "</td> <td>" +
-            "<a href='#'>" + body.title+"</a></td> <td>" + body.username + "</td><td>"+ "<a id='del'>x</a>" + "</td></tr>"
+        tableContent.innerHTML += "<tr><td>" + body.today + "</td> <td class='tit'>" +
+            body.title+"</td> <td>" + body.username + "</td><td>"+ "<a id='del'>x</a>" + "</td></tr>"
     }
-})
+});
