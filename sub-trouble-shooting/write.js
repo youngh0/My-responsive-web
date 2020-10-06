a34d0d5ba906e47b467c9a94daf11cdaa408ff07
youngho-C <cyh6099@gmail.com>
2020-10-06 16:12:01 +0900

    show기능 수정

--- sub-trouble-shooting/write.js
+++ sub-trouble-shooting/write.js
@@ -12,14 +12,14 @@ function login(){
 
 }
 // 데티어 불러와서 보여주기
-function show(){
+function show(wpahr){
     firebase.database().ref('board').once('value', function (snapshot) {
-        let test_box = document.querySelector("#test");
+        let test_box = document.querySelector("#show_box");
         const postData = Object.entries(snapshot.val());
         for(let i = 0; i < postData.length;i++)
         {
             const[key,body] = postData[i];
-            if(document.getElementById('num').value === body.title){
+            if(wpahr === body.title){
                 test_box.innerHTML = "<h1>" + body.title + "</h1><div id='bo'>" + body.main_txt +"</div>";
                 console.log("ok")
                 return;
@@ -123,7 +123,7 @@ firebase.database().ref('board').on('value', function (snapshot) {
     {
         const[key,body] = postData[i];
         console.log(body.title);
-        tableContent.innerHTML += "<tr><td>" + body.today + "</td> <td class='tit'>" +
-            body.title+"</td> <td>" + body.username + "</td><td>"+ "<a id='del'>x</a>" + "</td></tr>"
+        tableContent.innerHTML += "<tr><td>" + body.today + "</td> <td><a onclick='show(id)'id='"+ body.title  + "'> " +
+            body.title+"</a class='tit'></td> <td>" + body.username +"</td><td>"+ "<a id='"+body.title +"'>x</a></tr>"
     }
 });
