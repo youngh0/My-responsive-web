378d241e124b835332dc42b66f9aa746deb87640
youngho-C <cyh6099@gmail.com>
2020-10-08 20:53:02 +0900

    login 기능 구현

--- sub-trouble-shooting/login.js
+++ sub-trouble-shooting/login.js
@@ -1,4 +1,11 @@
 
+function hidebox(){
+    let box_up = document.querySelector('.main_sign_up');
+    let box_in = document.querySelector('.main_sign_in');
+    box_in.style.display = 'none';
+    box_up.style.display = 'none';
+}
+
 function show_sign_up(){
     let three_btn = document.querySelector('.func');
     let show_sign_up = document.querySelector('.main_sign_up');
@@ -23,11 +30,22 @@ function signUp(){
     })
 }
 
+function show_sign_in(){
+    let three_btn = document.querySelector('.func');
+    let show_sign_up = document.querySelector('.main_sign_in');
+    show_sign_up.style.display = 'block';
+    three_btn.style.display = "none";
+}
+
 function login(){
-    let id = document.querySelector('#email_in').value;
+    let show_sign_up = document.querySelector('.main_sign_in');
+    let three_btn = document.querySelector('.func');
+    let id = document.querySelector('#id_in').value;
     let pw = document.querySelector('#pw_in').value;
     firebase.auth().signInWithEmailAndPassword(id, pw).then(cred => {
         alert("o");
+        three_btn.style.display = "block"
+        show_sign_up.style.display = 'none';
         location.href = "trouble.html";
     }).catch(function (error) {
             var errorCode = error.code;
