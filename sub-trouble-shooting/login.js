db38ec68c12902943eb53a9fb16ecee49f25328e
youngho-C <cyh6099@gmail.com>
2020-10-08 20:31:56 +0900

    login 기능 구현

--- sub-trouble-shooting/login.js
+++ sub-trouble-shooting/login.js
@@ -1,19 +1,21 @@
 
-
+function show_sign_up(){
+    let three_btn = document.querySelector('.func');
+    let show_sign_up = document.querySelector('.main_sign_up');
+    show_sign_up.style.display = 'block';
+    three_btn.style.display = "none";
+}
 function signUp(){
-    // const signupForm = document.querySelector('#sign_up');
-    // signupForm.addEventListener('submit', (e)=>{
-    //     e.preventDefault();
-    //
-    //
-    // })
-    // console.log("finish")
+    let show_sign_up = document.querySelector('.main_sign_up');
+    let three_btn = document.querySelector('.func');
     let id = document.querySelector('#id').value;
     let pw = document.querySelector('#pw').value;
 
     firebase.auth().createUserWithEmailAndPassword(id,pw).then(cred =>{
         alert("o");
         location.href = "trouble.html"
+        three_btn.style.display = "block"
+        show_sign_up.style.display = 'none';
     }).catch(function (error){
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -22,5 +24,14 @@ function signUp(){
 }
 
 function login(){
-
+    let id = document.querySelector('#email_in').value;
+    let pw = document.querySelector('#pw_in').value;
+    firebase.auth().signInWithEmailAndPassword(id, pw).then(cred => {
+        alert("o");
+        location.href = "trouble.html";
+    }).catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            alert(errorMessage);
+    });
 }
